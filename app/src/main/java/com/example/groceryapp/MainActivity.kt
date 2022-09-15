4a2cdec5a40575a72e544cf1115edf7fdb26eae8
WestinMarcus <78566160+WestinMarcus@users.noreply.github.com>
2022-09-15 14:51:33 +0200

    Some buttonfunctionality

--- app/src/main/java/com/example/groceryapp/MainActivity.kt
+++ app/src/main/java/com/example/groceryapp/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.groceryapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        var loginFailedText = findViewById(R.id.textView4) as TextView
+        //get reference to button
+        val login_clicker = findViewById(R.id.button) as Button
+        login_clicker.setOnClickListener{
+            loginFailedText.text = "Login Failed"
+            Toast.makeText(this@MainActivity, "you clicked login.", Toast.LENGTH_SHORT).show()
+        }
     }
 }
