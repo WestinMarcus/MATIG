a25a5b96ab2847b486ec4a5fe149a48f61eb0e49
WestinMarcus <78566160+WestinMarcus@users.noreply.github.com>
2022-09-15 21:07:29 +0200

    added activity with some buttons, no functionality yet

--- app/src/main/java/com/example/groceryapp/MainActivity.kt
+++ app/src/main/java/com/example/groceryapp/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.groceryapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
@@ -12,12 +14,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
-        var loginFailedText = findViewById(R.id.textView4) as TextView
+        val loginFailedText = findViewById(R.id.textView4) as TextView
         //get reference to button
-        val login_clicker = findViewById(R.id.button) as Button
-        login_clicker.setOnClickListener{
+        val loginBtn = findViewById(R.id.button) as Button
+        loginBtn.setOnClickListener{
             loginFailedText.text = "Login Failed"
             Toast.makeText(this@MainActivity, "you clicked login.", Toast.LENGTH_SHORT).show()
+
+        }
+        val createUsrBtn = findViewById(R.id.button2) as Button
+        createUsrBtn.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
         }
     }
 }
