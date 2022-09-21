b6ca08dce3480d27124c4c5c7cffbc8b0bb054f4
Jacoberggren <jacoberggren@gmail.com>
2022-09-21 15:11:06 +0200

    storeList + storeAdressList

--- app/src/main/java/com/example/groceryapp/StoresActivity.kt
+++ app/src/main/java/com/example/groceryapp/StoresActivity.kt
@@ -17,7 +17,34 @@ class StoresActivity : AppCompatActivity() {
         //-------------------------------------------------
         //adds new users collection with new document containing a user
         val db = Firebase.firestore
+        val storeList = mutableListOf<String>()
+        val storeAdressList = mutableListOf<String>()
 
+        db.collection("store list").document("Butik info").collection("Coop")
+            .get()
+            // result gets all documents in collection
+            .addOnSuccessListener { result ->
+                for (document in result)
+                {
+                    storeList.add(document.id)
+                    storeAdressList.add(document.getString("Adress") ?: "default")
+
+                    Log.d(TAG, "${document.id} => ${document.data}")
+                }
+                // print storeList
+                for((index, storeName) in storeList.withIndex())
+                {
+                    Log.i(TAG, "store at index: $index is $storeName")
+                }
+
+                for((index, storeAdress) in storeAdressList.withIndex())
+                {
+                    Log.i(TAG, "store at index: $index is $storeAdress")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.w(TAG, "Error getting documents.", exception)
+            }
 
     }
 
