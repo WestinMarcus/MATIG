b2b4b5739aebaab954160336a101a754c87427f7
Jacoberggren <jacoberggren@gmail.com>
2022-09-21 09:15:16 +0200

    working get() from firebase

--- app/src/main/java/com/example/groceryapp/StoresActivity.kt
+++ app/src/main/java/com/example/groceryapp/StoresActivity.kt
@@ -1,13 +1,58 @@
 package com.example.groceryapp
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.firestore.ktx.firestore
 
 class StoresActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_stores)
+
+        //-------------------------------------------------
+        //adds new users collection with new document containing a user
+        val db = Firebase.firestore
+
+        // Create a new user with a first and last name
+        val user1 = hashMapOf(
+            "first" to "Ada",
+            "last" to "Lovelace",
+            "born" to 1815
+        )
+        val user2 = hashMapOf(
+            "first" to "Alan",
+            "middle" to "Mathison",
+            "last" to "Turing",
+            "born" to 1912
+        )
+        /*
+        // Add a new document with a generated ID
+        db.collection("users")
+            .add(user2)
+            .addOnSuccessListener { documentReference ->
+                Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+            }
+            .addOnFailureListener { e ->
+                Log.w(TAG, "Error adding document", e)
+            }
+        */
+        db.collection("store list").document("Butik info").collection("Coop")
+            .get()
+            // results gets all documents in collection
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    Log.d(TAG, "${document.id} => ${document.data}")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.w(TAG, "Error getting documents.", exception)
+            }
     }
+
+
 }
