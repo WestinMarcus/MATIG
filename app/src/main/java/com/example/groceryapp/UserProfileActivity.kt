807e11d61ab02186e07a5886c5ce3c403824be46
JakobML <jakobtaf@gmail.com>
2022-10-05 13:31:24 +0200

    userinfo display app complete

--- app/src/main/java/com/example/groceryapp/UserProfileActivity.kt
+++ app/src/main/java/com/example/groceryapp/UserProfileActivity.kt
@@ -1,10 +1,8 @@
 package com.example.groceryapp
 
-import android.content.ContentValues
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
@@ -47,25 +45,47 @@ class UserProfileActivity : AppCompatActivity() {
         }
 
         // Tar in textview
-        var textView : TextView = findViewById(R.id.firstNameField) as TextView
+        val firstName : TextView = findViewById(R.id.firstNameField) as TextView
+        val lastName : TextView = findViewById(R.id.userlastNamefield) as TextView
+        val dateOfBirth : TextView = findViewById(R.id.userDateOfBirth) as TextView
+        val phoneNmb : TextView = findViewById(R.id.userPhone) as TextView
+        val zip : TextView = findViewById(R.id.userZip) as TextView
+        val adress : TextView = findViewById(R.id.userAdress) as TextView
+        val city : TextView = findViewById(R.id.userCity) as TextView
 
         val user = Firebase.auth.currentUser
         val uid = user?.uid
         var userName = ""
+        var userLast = ""
+        var userDate = ""
+        var userPhone = ""
+        var userZip = ""
+        var userAdress = ""
+        var userCity = ""
 
         db.collection("users").document("$uid")
             .get()
             .addOnSuccessListener { document ->
                 userName = document.getString("First name") ?: "default"
-                textView.setText(userName).toString()
+                userLast = document.getString("Last name") ?: "default"
+                userDate = document.getString("Date of birth") ?: "default"
+                userPhone = document.getString("Phone number") ?: "default"
+                userZip = document.getString("Zip Code") ?: "default"
+                userAdress = document.getString("Adress") ?: "default"
+                userCity = document.getString("City") ?: "default"
+                firstName.setText(userName).toString()
+                lastName.setText(userLast).toString()
+                dateOfBirth.setText(userDate).toString()
+                phoneNmb.setText(userPhone).toString()
+                zip.setText(userZip).toString()
+                adress.setText(userAdress).toString()
+                city.setText(userCity).toString()
             }
 
-
-        //Sätter nytt värde på textview
     }
 
     private fun performUpdate() {
-
+    //kolla om man kan köra en "reload" av sidan vid uppdatering så att oncreate körs igen
 
     }
 }
