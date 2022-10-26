6640c2bff0559034b704a43beb6d000e2025a69a
Jacoberggren <jacoberggren@gmail.com>
2022-10-26 11:17:28 +0200

    FAQ Done

--- app/src/main/java/com/example/groceryapp/FAQActivity.kt
+++ app/src/main/java/com/example/groceryapp/FAQActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.ImageView
 import android.widget.ListView
+import android.widget.TextView
 
 class FAQActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,30 +13,33 @@ class FAQActivity : AppCompatActivity() {
         setContentView(R.layout.activity_faqactivity)
 
         val backBtn = findViewById<ImageView>(R.id.image_back_settings)
-
         backBtn.setOnClickListener {
             finish()
         }
-        val arrayAdapter: ArrayAdapter<*>
-        val faqListView = findViewById<ListView>(R.id.lv_faq)
-        val questionsAndAnswers = mutableListOf<String>()
 
-        // --------- Account questions ------------
-        questionsAndAnswers.add("How do i create an account?")
-        questionsAndAnswers.add("Do i need an account?")
-        questionsAndAnswers.add("How do i change password?")
-        // --------- Product questions -------------
-        questionsAndAnswers.add("When are the deals updated?")
-        questionsAndAnswers.add("Which stores are used in the app?")
-        questionsAndAnswers.add("How do i find the best price for a product?")
-        // -------- placeholder questions -----------
-        questionsAndAnswers.add("Can I add my own products to the shoppinglist?")
-        questionsAndAnswers.add("How do I create/ delete a shoppinglist?")
-        questionsAndAnswers.add("Can I see products I've purchased in the past?")
+        val q1Tv = findViewById<TextView>(R.id.tv_question1)
+        val q2Tv = findViewById<TextView>(R.id.tv_question2)
+        val q3Tv = findViewById<TextView>(R.id.tv_question3)
 
+        val a1Tv = findViewById<TextView>(R.id.tv_answer1)
+        val a2Tv = findViewById<TextView>(R.id.tv_answer2)
+        val a3Tv = findViewById<TextView>(R.id.tv_answer3)
 
-        arrayAdapter = ArrayAdapter(this, android.R.layout.simple_expandable_list_item_1, questionsAndAnswers)
-        faqListView.adapter = arrayAdapter
+        //------------------ questions -------------------
+        val q1 = "When are the deals updated?"
+        val q2 = "Can I see products I've purchased in the past?"
+        val q3 = "How do i change my password?"
+        //------------------- answers --------------------
+        val a1 = "Every Monday at 8am"
+        val a2 = "Yes you can! Click the Settings button in the Navigation bar -> Purchase History button"
+        val a3 = "Click the Settings button in the Navigation bar -> User Profile button -> Change Password. Proceed and follow the instructions in the email sent to your registered email address."
+
+        q1Tv.setText(q1)
+        a1Tv.setText(a1)
+        q2Tv.setText(q2)
+        a2Tv.setText(a2)
+        q3Tv.setText(q3)
+        a3Tv.setText(a3)
 
 
 
